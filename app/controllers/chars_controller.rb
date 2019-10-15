380de235a389977c4c3018244a63553ded7a1a25
Przemyslaw Prencel <przemyslaw.prencel@goodylabs.com>
2019-10-15 17:20:38 +0200

    string api implementation

--- app/controllers/chars_controller.rb
+++ app/controllers/chars_controller.rb
@@ -1,2 +1,24 @@
 class CharsController < ApplicationController
+
+  def chars_recognize
+    resp = ""
+
+    if params["str"].present?
+      chars = params["str"].split("")
+      resp = {
+        lowerCase: chars.select { |c| /[[:lower:]]/.match(c) },
+        upperCase: chars.select { |c| /[[:upper:]]/.match(c) },
+        digits: chars.select { |c| /[\d]/.match(c) },
+        special: chars.select { |c| /[_@?¿!#$%^&*=+€-]/.match(c) },
+      }
+    end
+
+    render json: {
+      charsTypes: resp,
+      errors: [
+        params["str"].nil? ? "No parameters given" : ""
+      ].reject(&:blank?)
+    }
+  end
+
 end
